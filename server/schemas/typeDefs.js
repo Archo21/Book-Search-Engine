081dacc8c61f574bfa42fe7cfd0f116e419bb1dc
Archo21 <87740574+Archo21@users.noreply.github.com>
2022-01-26 21:18:18 -0800

    Updated graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,6 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-type Mutation {
-  login(email: String!, password: String!): Auth
-  addUser(username: String!, email: String!, password: String!): Auth
-  saveBook(bookData: BookInput!): User
-  removeBook(bookId: ID!): User
-}
 type User {
     _id: ID!
     username: String!
@@ -22,6 +16,10 @@ type Book {
     link: String
     title: String!
   }
+  type Auth {
+    token: ID!
+    user: User
+  }
 input BookInput {
     authors: [String]
     description: String!
@@ -30,13 +28,18 @@ input BookInput {
     link: String
     title: String!
   }
+  
 type Query {
     me: User
   }
-  type Auth {
-    token: ID!
-    user: User
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
+
 
 module.exports = typeDefs;
